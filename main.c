3c758fc0c31774c2ba8aabfdbf49094c929f6d54
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-24 15:07:52 -0500

    2

---
+++ main.c
@@ -0,0 +1,21 @@
+#Luisa Aimoli 10169687
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlin.h>
+
+int main () {
+	char course[5] = "CISC";
+	char number[4] = "220";
+	char stri[10];
+
+char* s = safestrcat(course, number);
+printf("safestrcat(course, number) returns: %s\n", s);
+
+int s2 = substring(s, 2, 3, stri);
+printf("substring(s, 2, 3, stri) returns: %d\n", s2);
+
+free(s)
+
+return 0;
+
---
+++ mystring.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlin.h>
+
+char* safestrcat( char *a, char *b) {
+	int alen = strlen(a);
+	int blen = strlen(b);
+	char *new = malloc(alen+blen+1); //1 for the /0 character
+	strcpy(new, a);
+	strcat(new, b);
+	return new;
+}
+
+int substring(char *source, int from, int n, char *target) {
+	char *temp;
+	strcpy(temp, target);
+	int count = 0;
+	int length = strlen(source);
+
+	for (i=0; i < length; ++i) {
+		if (i >= from) {
+			if (count <= n) {		
+				strcat(target, source[i]);
+			}
+		}
+		++count;
+	}
+
+	if (strcmp(temp, target) == 0) { //subtring string is still the same as before
+		return -1; //failure 
+	}
+	return 0; //success
+}
